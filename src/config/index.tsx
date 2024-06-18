9bba64d4dbdc8e4f81dd386c1ee7a7627154285f
Kifoxive <palianycia@seznam.cz>
2024-06-18 18:04:14 +0200

    Updated vite mode

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,3 +1,5 @@
+console.log(process.env);
+
 export const config = {
     app: {
         url: 'http://localhost:5173',
@@ -8,7 +10,7 @@ export const config = {
         // url: 'http://localhost:3001/api',
         // production server
         // url: 'https://fairytale-be.netlify.app/api',
-        url: process.env.MODE === 'main' ? 'https://fairytale-be.netlify.app/api' : 'http://localhost:3001/api',
+        url: process.env.VITE_MODE === 'main' ? 'https://fairytale-be.netlify.app/api' : 'http://localhost:3001/api',
         // mapboxToken: 'pk.eyJ1IjoiaHVsZGFjeiIsImEiOiJjbGJ6M2l3d2cwd2hyM3FvNXM5M2twb2RlIn0.nVPfoiec06LkIocNYRPoGA',
         // websocketUrl:
         //     process.env.VITE_MODE === 'main' ? 'wss://api.qapline.k42.app' : 'wss://dev.qapline.api.koala42.com',
